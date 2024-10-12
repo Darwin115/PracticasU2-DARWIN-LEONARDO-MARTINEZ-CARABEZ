03abfd421b771ca7c17ca9dbd53c8454c0dfba7b
Darwin115 <darwinleonardo111@gmail.com>
2024-10-11 22:16:13 -0600

    Act

---
+++ Practica 3/src/screens/HomeScreen.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet } from 'react-native';
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={styles.container}>
+      <Text>Home Screen</Text>
+      <Text> </Text>
+      <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Details')}>
+        <Text style={styles.buttonText}>Go to Details</Text>
+      </TouchableOpacity>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default HomeScreen;
